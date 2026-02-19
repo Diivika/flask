b3438780676a16dbfea8bf4dda9a19915e731b2b
Diivika <diivika2@yandex.ru>
2026-02-19 17:06:36 +0300

    update

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def name():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def promotion():
+    adds_list = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
+                 'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
+    return '</br>'.join(adds_list)
+
+
+@app.route('/image_mars')
+def image_mars():
+    return """<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="/static/images/MARS.png"/>
+                        <figcaption>Вот она какая, красная планета</figcaption>
+                      </body>
+                    </html>"""
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
