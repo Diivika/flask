fec0f5cd09932a8f56f716e75953695c8117fc0f
Diivika <diivika2@yandex.ru>
2026-02-26 17:29:08 +0300

    patchnote

--- main_lesson2.py
+++ main_lesson2.py
@@ -10,9 +10,15 @@ def index():
     user = "Ученик Яндекс.Лицея"
     return render_template('base.html', title='Заготовка')
 
+
 @app.route('/training/<prof>')
 def training(prof):
     return render_template('base.html', proffesion=prof)
+
+
+@app.route('/list_prof/<list>')
+def spisok(list):
+    return render_template('base.html', how=list)
 
 
 if __name__ == '__main__':
