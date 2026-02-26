7018bd5d0676049e5e0282122f5697b58c19b6da
Diivika <diivika2@yandex.ru>
2026-02-26 16:28:10 +0300

    add lesson 2

---
+++ main_lesson2.py
@@ -0,0 +1,14 @@
+from flask import Flask, url_for, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = "Ученик Яндекс.Лицея"
+    return render_template('base.html', title='Заготовка')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
