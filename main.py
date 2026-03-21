e11e816945446d0446db52c6589ecdd8647b0e82
Diivika <vasdima4466@gmail.com>
2026-03-21 11:35:00 +0300

    add register

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, redirect
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
 from add_users import insert_users
 from add_jobs import insert_jobs
+from forms.register_user import RegisterForm
 
 db_sess = db_session.create_session()
 db_session.global_init("db/mars_explorer.db")
@@ -394,6 +395,38 @@ def table(gender, age):
     param['gender'] = gender
     param['age'] = age
     return render_template('table.html', **param)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        try:
+            user = User(
+                email=form.email.data,
+                surname=form.surname.data,
+                name=form.name.data,
+                age=form.age.data,
+                position=form.position.data,
+                speciality=form.speciality.data,
+                address=form.address.data,
+            )
+            user.set_password(form.password.data)
+            db_sess.add(user)
+            db_sess.commit()
+            return 'success'
+        except Exception:
+            return 'Oops, something dont work'
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == '__main__':
