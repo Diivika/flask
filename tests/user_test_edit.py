1a9a6c94e07b5927fbb57c4b465205c6e098a0c0
Diivika <vasdima4466@gmail.com>
2026-03-30 23:15:55 +0300

    update tests

--- tests/user_test_edit.py
+++ tests/user_test_edit.py
@@ -9,9 +9,9 @@ print(put('http://localhost:8080/api/users/2',
            json={'surname': 'Sigmason',
                  'name': 'Sanya',
                  'age': 77,
-                 'position': 'pilot-2',
+                 'position': 'pilot-3',
                  'email': 'superpilot2@mars.space',
-                 'address': 'module_2',
+                 'address': 'module_3',
                  'password': 'sigmawork2',
                  'speciality': 'pilot_support',
                  'city_from': 'Samara'}).json())
@@ -25,7 +25,7 @@ print(put('http://localhost:8080/api/v2/users/1', json={}).json())
 print(put('http://localhost:8080/api/v2/users/1',
            json={'name': 'asfafafasfasfas'}).json())
 
-print(put('http://localhost:8080/api/v2/users/3',
+print(put('http://localhost:8080/api/v2/users/1',
            json={'surname': 'Sigmason',
                  'name': 'Sanya',
                  'age': 77,
